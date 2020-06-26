b67f68fe1dd915643b5a6fa3fd97d4619d9112e4
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-06-25 23:03:10 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace divis_o
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o numerador...");
+            String numerador = Console.ReadLine();
+            Decimal numerador1 = Convert.ToDecimal(numerador);
+
+            Console.WriteLine("digite o denominador...");
+            String denominador = Console.ReadLine();
+            Decimal denominador1 = Convert.ToDecimal(denominador);
+
+            Decimal resultado = numerador1 / denominador1;
+            Console.WriteLine("o resultado da divisão é ...." + resultado);
+
+
+        }
+    }
+}
